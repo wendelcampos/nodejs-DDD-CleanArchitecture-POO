4a29516331d92ba4a329ece1e448c5425747c4c9
Wendel Campos Aguiar <79800240+wendelcampos@users.noreply.github.com>
2025-05-06 17:59:47 -0300

    test: add use case edit question

---
+++ src/domain/forum/application/useCases/edit-question.spec.ts
@@ -0,0 +1,52 @@
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository';
+import { makeQuestion } from 'test/factories/make-question';
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+import { EditQuestionUseCase } from './edit-question';
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let sut: EditQuestionUseCase
+
+describe('Edit Question', () => {
+    beforeEach(() => {
+        inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+        sut = new EditQuestionUseCase(inMemoryQuestionsRepository)
+    })
+
+    it('should be able to edit a question', async () => {
+        const newQuestion = makeQuestion({
+            authorId: new UniqueEntityID('author-1')
+        }, new UniqueEntityID('question-1'))
+
+        await inMemoryQuestionsRepository.create(newQuestion)
+    
+        await sut.execute({
+            questionId: newQuestion.id.toString(),
+            authorId: 'author-1',
+            title: 'New title',
+            content: 'New content'
+        })
+    
+        expect(inMemoryQuestionsRepository.items[0]).toMatchObject({
+            title: 'New title',
+            content: 'New content'
+        })
+    })
+
+    it('should not be able to edit a question from another user', async () => {
+        const newQuestion = makeQuestion({
+            authorId: new UniqueEntityID('author-1')
+        }, new UniqueEntityID('question-1'))
+
+        await inMemoryQuestionsRepository.create(newQuestion)
+    
+        await expect(() => {
+            return sut.execute({
+                questionId: newQuestion.id.toString(),
+                authorId: 'author-2',
+                title: 'New title',
+                content: 'New content'
+            })
+        }).rejects.toBeInstanceOf(Error)
+    })
+});
+
