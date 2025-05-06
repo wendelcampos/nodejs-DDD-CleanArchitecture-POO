c0ef51f579afbc625a78d1c6c49acb6e714f35dd
Wendel Campos Aguiar <79800240+wendelcampos@users.noreply.github.com>
2025-05-06 17:59:32 -0300

    feat: add use case edit question

--- src/domain/forum/application/repositories/questions-repository.ts
+++ src/domain/forum/application/repositories/questions-repository.ts
@@ -2,6 +2,7 @@ import { Question } from "../../enterprise/entities/question";
 export interface QuestionsRepository {
     findById(id: string): Promise<Question | null>
     findBySlug(slug: string): Promise<Question | null>
+    save(question: Question): Promise<void>
     create(question: Question): Promise<void>
     delete(question: Question): Promise<void>
 }
