dfc5e74d6793cf26423e9a2a1d836bdd73f1671f
Wendel Campos Aguiar <79800240+wendelcampos@users.noreply.github.com>
2025-05-06 17:41:13 -0300

    feat: add use case delete answer

--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -1,10 +1,28 @@
 import { AnswersRepository } from "@/domain/forum/application/repositories/answers-repository";
 import { Answer } from "@/domain/forum/enterprise/entities/answer";
-
 export class InMemoryAnswersRepository implements AnswersRepository {
+    
     public items: Answer[] = [];
+
+    async findById(id: string): Promise<Answer | null> {
+        const answer = this.items.find((item) => item.id.toString() === id);
+
+        if (!answer) {
+            return null
+        }
+
+        return answer;
+    }
 
     async create(answer: Answer) {
         this.items.push(answer);
+    }
+
+    async delete(answer: Answer): Promise<void> {
+        const itemIndex = this.items.findIndex((item) => item.id === answer.id);
+
+        if (itemIndex >= 0) {
+            this.items.splice(itemIndex, 1);
+        }
     }
 }
