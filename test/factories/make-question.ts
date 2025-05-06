50c9aaac960e8206ecc2bd0bd3ef5f054a277c91
Wendel Campos Aguiar <79800240+wendelcampos@users.noreply.github.com>
2025-05-06 13:28:26 -0300

    feat: add new library fake-js

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -1,13 +1,15 @@
+import { faker } from "@faker-js/faker";
+
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { Question, QuestionProps } from "@/domain/forum/enterprise/entities/question";
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
+export function makeQuestion(override: Partial<QuestionProps> = {}, id? : UniqueEntityID) {
     const question =  Question.create({
         authorId: new UniqueEntityID('1'),
-        title: 'Example Question',
-        content: 'Example content',
+        title: faker.lorem.sentence(10),
+        content: faker.lorem.text(),
         ...override
-    })
+    }, id)
     
     return question
 }
