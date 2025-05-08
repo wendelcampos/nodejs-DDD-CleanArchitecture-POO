20eb2e6b9e20ef7adedc6c45b6663ce636bcfa62
Wendel Campos Aguiar <79800240+wendelcampos@users.noreply.github.com>
2025-05-08 16:54:22 -0300

    test: add use case fetch answer comments

---
+++ src/domain/forum/application/useCases/fetch-answer-comments.spec.ts
@@ -0,0 +1,43 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+import { InMemoryAnswerCommentsRepository } from 'test/repositories/in-memory-answer-comment-repository';
+import { FetchAnswerCommentsUseCase } from './fetch-answer-comments';
+import { makeAnswerComment } from 'test/factories/make-answer-comment';
+
+let inMemoryAnswersCommentsRepository: InMemoryAnswerCommentsRepository
+let sut: FetchAnswerCommentsUseCase
+
+describe('Fetch Answer Answers', () => {
+    beforeEach(() => {
+        inMemoryAnswersCommentsRepository = new InMemoryAnswerCommentsRepository()
+        sut = new FetchAnswerCommentsUseCase(inMemoryAnswersCommentsRepository)
+    })
+
+    it('should be able to fetch question answers', async () => {
+        await inMemoryAnswersCommentsRepository.create(makeAnswerComment({ answerId: new UniqueEntityID('question-1') }))
+        await inMemoryAnswersCommentsRepository.create(makeAnswerComment({ answerId: new UniqueEntityID('question-1') }))
+        await inMemoryAnswersCommentsRepository.create(makeAnswerComment({ answerId: new UniqueEntityID('question-1') }))
+
+        const { answerComments } = await sut.execute({
+            answerId: 'question-1',
+            page: 1,
+        })
+
+        expect(answerComments).toHaveLength(3)
+    });
+    
+    it('should be able to fetch paginated question answers', async () => {
+        for (let i = 1; i <= 22; i++) {
+            await inMemoryAnswersCommentsRepository.create(makeAnswerComment({
+                answerId: new UniqueEntityID('question-1'),
+            }))
+        }
+
+        const { answerComments } = await sut.execute({
+            answerId: 'question-1',
+            page: 2,
+        })
+
+       expect(answerComments).toHaveLength(2)
+    });
+
+})
