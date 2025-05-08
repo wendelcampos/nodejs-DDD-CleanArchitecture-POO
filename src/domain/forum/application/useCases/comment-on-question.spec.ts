3c28a0e08a2e1282a3cd6159c55e1e815f947030
Wendel Campos Aguiar <79800240+wendelcampos@users.noreply.github.com>
2025-05-08 15:14:56 -0300

    test: add use case comment on question

---
+++ src/domain/forum/application/useCases/comment-on-question.spec.ts
@@ -0,0 +1,37 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+import { makeAnswer } from 'test/factories/make-answer';
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository';
+import { makeQuestion } from 'test/factories/make-question';
+import { InMemoryQuestionCommentsRepository } from 'test/repositories/in-memory-question-comments-repository';
+import { CommentOnQuestionUseCase } from './comment-on-question';
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let inMemoryQuestionCommentsRepository: InMemoryQuestionCommentsRepository
+let sut: CommentOnQuestionUseCase
+
+describe('Choose Question Best Answer', () => {
+    beforeEach(() => {
+        inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+        inMemoryQuestionCommentsRepository = new InMemoryQuestionCommentsRepository()
+
+        sut = new CommentOnQuestionUseCase(
+            inMemoryQuestionsRepository,
+            inMemoryQuestionCommentsRepository
+        )
+    })
+
+    it('should be able to comment on question', async () => {
+        const question = makeQuestion()
+
+        await inMemoryQuestionsRepository.create(question)
+    
+        await sut.execute({
+            questionId: question.id.toString(),
+            authorId: question.authorId.toString(),
+            content: 'New comment'
+        })
+    
+        expect(inMemoryQuestionCommentsRepository.items[0].content).toEqual('New comment')
+    })
+});
+
