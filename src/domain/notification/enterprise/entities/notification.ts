8557dd0358c92004138f87caec122b9920132a9a
Wendel Campos Aguiar <79800240+wendelcampos@users.noreply.github.com>
2025-05-22 16:45:07 -0300

    feat: created subdomain notification and created entitie notification

---
+++ src/domain/notification/enterprise/entities/notification.ts
@@ -0,0 +1,46 @@
+import { Entity } from "@/core/entities/entity";
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+import { Optional } from "@/core/types/optional";
+
+export interface NotificationProps {
+    recipientId: UniqueEntityID
+    title: string;
+    content: string;
+    createAt: Date;
+    readAt?: Date;
+}
+
+export class Notification extends Entity<NotificationProps> {
+    get recipientId() {
+        return this.props.recipientId;
+    }
+
+    get title() {
+        return this.props.title;
+    }
+
+    get content() {
+        return this.props.content;
+    }
+
+    get createAt() {
+        return this.props.createAt;
+    }
+
+    get readAt() {
+        return this.props.readAt;
+    }
+
+    static create(props: Optional<NotificationProps, "createAt">, id?: UniqueEntityID) {
+        const notification = new Notification(
+            {
+                ...props,
+                createAt: props.createAt ?? new Date(),
+            },
+            id
+        );
+
+        return notification;
+
+    }
+}
