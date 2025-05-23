2f28383a07a804daf2e0a73cdddfb5669f87b381
Wendel Campos Aguiar <79800240+wendelcampos@users.noreply.github.com>
2025-05-23 11:40:24 -0300

    test: created use case send notification

---
+++ src/domain/notification/application/use-cases/send-notification.spec.ts
@@ -0,0 +1,26 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+import { InMemoryNotificationsRepository } from 'test/repositories/in-memory-notifications-repository';
+import { SendNotificationUseCase } from './send-notification';
+
+let inMemoryNotificationsRepository: InMemoryNotificationsRepository
+let sut: SendNotificationUseCase
+
+describe('Send Notification', () => {
+    beforeEach(() => {
+        inMemoryNotificationsRepository = new InMemoryNotificationsRepository()
+        sut = new SendNotificationUseCase(inMemoryNotificationsRepository)
+    })
+
+    it('should be able to create a notification', async () => {
+    
+        const result = await sut.execute({
+            recipientId: '1',
+            title: 'New Notification',
+            content: 'Content of the notification',
+        })
+    
+        expect(result.isRight()).toBe(true)
+        expect(inMemoryNotificationsRepository.items[0]).toEqual(result.value?.notification)
+    })
+});
+
